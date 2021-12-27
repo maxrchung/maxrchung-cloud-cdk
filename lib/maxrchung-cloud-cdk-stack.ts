3901cb1f2518df0c18d827b33f703bc624d9306d
maxrchung <maxrchung@gmail.com>
2021-12-26 23:43:48 -0800

    add backup plan

--- lib/maxrchung-cloud-cdk-stack.ts
+++ lib/maxrchung-cloud-cdk-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
+import * as backup from '@aws-cdk/aws-backup';
 // import * as sqs from '@aws-cdk/aws-sqs';
 
 // https://bobbyhadz.com/blog/aws-cdk-ec2-instance-example
@@ -12,7 +13,7 @@ export class MaxrchungCloudCdkStack extends cdk.Stack {
     super(scope, id, props);
 
     const vpc = new ec2.Vpc(this, 'cloud-vpc', {
-      natGateways: 0
+      natGateways: 0,
     });
 
     const securityGroup = new ec2.SecurityGroup(this, 'database-security-group', {
@@ -38,10 +39,10 @@ export class MaxrchungCloudCdkStack extends cdk.Stack {
       'mongo',
     );
 
-    new ec2.Instance(this, 'database-ec2', {
+    const ec2Instance = new ec2.Instance(this, 'database-ec2', {
       vpc,
       vpcSubnets: {
-        subnetType: ec2.SubnetType.PUBLIC
+        subnetType: ec2.SubnetType.PUBLIC,
       },
       securityGroup,
       instanceType: ec2.InstanceType.of(
@@ -52,7 +53,14 @@ export class MaxrchungCloudCdkStack extends cdk.Stack {
         generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
         cpuType: ec2.AmazonLinuxCpuType.ARM_64,
       }),
-      keyName: 'cloud-key',
+      keyName: 'cloud-key', // Key was manually created through console
+    });
+
+    const backupPlan = backup.BackupPlan.daily35DayRetention(this, 'database-backup-plan');
+    backupPlan.addSelection('database-backup-selection', {
+      resources: [
+        backup.BackupResource.fromEc2Instance(ec2Instance),
+      ],
     });
 
     // The code that defines your stack goes here
