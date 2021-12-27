127368596fd5e690cd7acf16d23a3a792da6055a
maxrchung <maxrchung@gmail.com>
2021-12-27 01:44:08 -0800

    add database backup bucket

--- lib/maxrchung-cloud-cdk-stack.ts
+++ lib/maxrchung-cloud-cdk-stack.ts
@@ -1,16 +1,18 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as s3 from '@aws-cdk/aws-s3';
 
 export class MaxrchungCloudCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const vpc = new ec2.Vpc(this, 'cloud-vpc', {
+      vpcName: 'cloud-vpc',
       natGateways: 0,
     });
 
     const securityGroup = new ec2.SecurityGroup(this, 'database-security-group', {
+      securityGroupName: 'database-security-group',
       vpc,
       allowAllOutbound: true,
     });
@@ -33,7 +35,8 @@ export class MaxrchungCloudCdkStack extends cdk.Stack {
       'mongo',
     );
 
-    const ec2Instance = new ec2.Instance(this, 'database-ec2', {
+    new ec2.Instance(this, 'database-ec2', {
+      instanceName: 'database-ec2',
       vpc,
       vpcSubnets: {
         subnetType: ec2.SubnetType.PUBLIC,
@@ -50,11 +53,11 @@ export class MaxrchungCloudCdkStack extends cdk.Stack {
       keyName: 'cloud-key', // Key was manually created through console
     });
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'MaxrchungCloudCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new s3.Bucket(this, 'database-backup', {
+      bucketName: 'maxrchung-database-backup', // Has to be globally unique
+      lifecycleRules: [{
+        expiration: cdk.Duration.days(35)
+      }]
+    });
   }
 }
