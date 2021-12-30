ecdff5822ee11fea8d8f42b60a16090d51a001ec
maxrchung <maxrchung@gmail.com>
2021-12-30 01:54:16 -0800

    successfully deploy fargate and friends

--- lib/maxrchung-cloud-cdk-stack.ts
+++ lib/maxrchung-cloud-cdk-stack.ts
@@ -5,13 +5,8 @@ import * as route53 from '@aws-cdk/aws-route53';
 import * as certificatemanager from '@aws-cdk/aws-certificatemanager';
 import * as elbv2 from '@aws-cdk/aws-elasticloadbalancingv2';
 import * as ecs from '@aws-cdk/aws-ecs';
+import * as targets from '@aws-cdk/aws-route53-targets';
 import * as ssm from '@aws-cdk/aws-ssm';
-
-const getParameter = (scope: cdk.Construct, parameterName: string) => 
-  ssm.StringParameter.fromSecureStringParameterAttributes(scope, parameterName, {
-    parameterName,
-    version: 1,
-  }).stringValue
 
 export class MaxrchungCloudCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -65,31 +60,39 @@ export class MaxrchungCloudCdkStack extends cdk.Stack {
     });
 
     new ec2.CfnEIP(this, 'database-elastic-ip', {
-      instanceId: ec2Instance.instanceId
+      instanceId: ec2Instance.instanceId,
     });
 
     new s3.Bucket(this, 'database-backup', {
       bucketName: 'maxrchung-database-backup', // Has to be globally unique
       lifecycleRules: [{
-        expiration: cdk.Duration.days(35)
-      }]
+        expiration: cdk.Duration.days(35),
+      }],
+    });
+
+    const balancer = new elbv2.ApplicationLoadBalancer(this, `cloud-application-load-balancer`, {
+      loadBalancerName: 'cloud-application-load-balancer',
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC
+      },
+      internetFacing: true,
     });
 
     const hostedZone = new route53.PublicHostedZone(this, 'maxrchung-hosted-zone', {
       zoneName: 'maxrchung.com',
     });
 
-    const certificate = new certificatemanager.Certificate(this, 'maxrchung-certificate', {
-      domainName: 'maxrchung.com',
-      validation: certificatemanager.CertificateValidation.fromDns(hostedZone),
+    new route53.ARecord(this, 'maxrchung-a-record', {
+      recordName: 'maxrchung.com',
+      zone: hostedZone,
+      target: route53.RecordTarget.fromAlias(new targets.LoadBalancerTarget(balancer)),
     });
 
-    const balancer = new elbv2.ApplicationLoadBalancer(this, `cloud-application-load-balancer`, {
-      vpc,
-      vpcSubnets: {
-        subnetType: ec2.SubnetType.PUBLIC
-      },
-      internetFacing: true,
+    new route53.CnameRecord(this, 'maxrchung-cname-record', {
+      recordName: '*.maxrchung.com', // Forwards all subdomains
+      zone: hostedZone,
+      domainName: 'maxrchung.com',
     });
 
     const balancerSecurityGroup = new ec2.SecurityGroup(this, 'cloud-balancer-security-group', {
@@ -105,35 +108,45 @@ export class MaxrchungCloudCdkStack extends cdk.Stack {
 
     balancer.addSecurityGroup(balancerSecurityGroup);
 
-    const listener = balancer.addListener('cloud-balancer-listener', {
-      open: true,
-      port: 443,
-      certificates: [
-        certificate
-      ],
+    const certificate = new certificatemanager.Certificate(this, 'maxrchung-certificate', {
+      domainName: 'maxrchung.com',
+      validation: certificatemanager.CertificateValidation.fromDns(hostedZone),
+      subjectAlternativeNames: [
+        'maxrchung.com',
+        '*.maxrchung.com',
+      ]
     });
 
-    const cluster = new ecs.Cluster(this, 'cloud-cluster', {
-      clusterName: 'cloud-cluster',
-      vpc,
-    });
-
-    const targetGroup = new elbv2.ApplicationTargetGroup(this, 'maxrchung-target-group', {
+    const targetGroup = new elbv2.ApplicationTargetGroup(this, 'maxrchung-rails-target-group', {
+      targetGroupName: 'maxrchung-rails-target-group',
       port: 3000,
       vpc,
       protocol: elbv2.ApplicationProtocol.HTTP,
     });
 
-    listener.addAction('maxrchung-rails-action', {
-      priority: 5,
-      conditions: [
-        elbv2.ListenerCondition.hostHeaders([
-          'maxrchung.com'
-        ]),
+   balancer.addListener('cloud-balancer-listener-https', {
+      open: true,
+      protocol: elbv2.ApplicationProtocol.HTTPS,
+      certificates: [
+        certificate,
       ],
-      action: elbv2.ListenerAction.forward([
+      defaultTargetGroups: [
         targetGroup,
-      ]),
+      ],
+    });
+
+    balancer.addListener('cloud-balancer-listener-http', {
+      open: true,
+      protocol: elbv2.ApplicationProtocol.HTTP,
+      defaultAction: elbv2.ListenerAction.redirect({
+        protocol: elbv2.ApplicationProtocol.HTTPS,
+        port: '443',
+      }),
+    });
+
+    const cluster = new ecs.Cluster(this, 'cloud-cluster', {
+      clusterName: 'cloud-cluster',
+      vpc,
     });
 
     const taskDefinition = new ecs.TaskDefinition(this, 'maxrchung-rails-task', {
@@ -147,40 +160,46 @@ export class MaxrchungCloudCdkStack extends cdk.Stack {
       containerName: 'maxrchung-rails-container',
       image: ecs.ContainerImage.fromRegistry('maxrchung/maxrchung-rails'),
       environment: {
-        AWS_ACCESS_KEY_ID: getParameter(this, 'maxrchung-aws-access-key-id'),
-        AWS_DEFAULT_REGION: getParameter(this, 'maxrchung-aws-default-region'),
-        AWS_SECRET_ACCESS_KEY: getParameter(this, 'maxrchung-aws-secret-access-key'),
-        DATABASE_HOST: getParameter(this, 'cloud-database-host'),
-        DATABASE_PASSWORD: getParameter(this, 'cloud-database-password'),
-        SECRET_KEY_BASE: getParameter(this, 'maxrchung-rails-secret-key-base'),
+        AWS_ACCESS_KEY_ID: ssm.StringParameter.valueForStringParameter(this, 'maxrchung-aws-access-key-id'),
+        AWS_DEFAULT_REGION: ssm.StringParameter.valueForStringParameter(this, 'maxrchung-aws-default-region'),
+        AWS_SECRET_ACCESS_KEY: ssm.StringParameter.valueForStringParameter(this, 'maxrchung-aws-secret-access-key'),
+        DATABASE_HOST: ssm.StringParameter.valueForStringParameter(this, 'cloud-database-host'),
+        DATABASE_PASSWORD: ssm.StringParameter.valueForStringParameter(this, 'cloud-database-password'),
+        SECRET_KEY_BASE: ssm.StringParameter.valueForStringParameter(this, 'maxrchung-rails-secret-key-base'),
       },
     });
 
     container.addPortMappings({ containerPort: 3000 });
 
-    // const fargateSecurityGroup = new ec2.SecurityGroup(this, 'maxrchung-rails-fargate-security-group', {
-    //   securityGroupName: 'maxrchung-rails-fargate-security-group',
-    //   vpc,
-    //   allowAllOutbound: true,
-    // });
+    // Only allow fargate to talk with load balancer
+    const fargateSecurityGroup = new ec2.SecurityGroup(this, 'maxrchung-rails-fargate-security-group', {
+      securityGroupName: 'maxrchung-rails-fargate-security-group',
+      vpc,
+    });
     
-    // fargateSecurityGroup.connections.allowFrom(
-    //   balancerSecurityGroup,
-    //   ec2.Port.allTcp(),
-    //   'application load balancer'
-    // );
-    
+    fargateSecurityGroup.connections.allowFrom(
+      balancerSecurityGroup,
+      ec2.Port.allTcp(),
+      'application load balancer'
+    );
+
+    fargateSecurityGroup.connections.allowTo(
+      balancerSecurityGroup,
+      ec2.Port.allTcp(),
+      'application load balancer'
+    );
+
     const fargate = new ecs.FargateService(this, 'maxrchung-rails-fargate', {
-      serviceName: 'maxrchung-rails-service',
+      serviceName: 'maxrchung-rails-fargate',
       cluster,
       desiredCount: 1,
       taskDefinition,
-      // securityGroups: [
-      //   fargateSecurityGroup
-      // ],
+      securityGroups: [
+        fargateSecurityGroup
+      ],
       assignPublicIp: true,
     });
 
-    fargate.attachToApplicationTargetGroup(targetGroup);
+    targetGroup.addTarget(fargate);
   }
 }
