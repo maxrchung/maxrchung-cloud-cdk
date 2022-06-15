5d01fb18b7c9ce442b64c505dd7483be76e0a0ab
maxrchung <maxrchung@gmail.com>
2022-06-15 01:18:47 -0700

    servicediscovery fargate

--- lib/maxrchung-cloud-cdk-stack.ts
+++ lib/maxrchung-cloud-cdk-stack.ts
@@ -10,6 +10,7 @@ import * as logs from '@aws-cdk/aws-logs'
 import * as ssm from '@aws-cdk/aws-ssm'
 import * as backup from '@aws-cdk/aws-backup'
 import * as events from '@aws-cdk/aws-events'
+import * as servicediscovery from '@aws-cdk/aws-servicediscovery'
 
 export class MaxrchungCloudCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -20,6 +21,11 @@ export class MaxrchungCloudCdkStack extends cdk.Stack {
       natGateways: 0
     })
 
+    // Manually maintaining EC2 that contains Postgres/Mongo databases and NGINX proxy
+    // Decided to run these manually since it's much cheaper to run compared to AWS equivalent services (RDS/DocumentDB, ALB)
+    // I had issues trying to specify EC2 through CDK since this is stateful, ran into problems where EC2 would be recreated because
+    // I didn't specify a specific AMI
+
     const databaseSecurityGroup = new ec2.SecurityGroup(this, 'database-security-group', {
       securityGroupName: 'database-security-group',
       vpc,
@@ -27,47 +33,25 @@ export class MaxrchungCloudCdkStack extends cdk.Stack {
     })
 
     databaseSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'ssh')
+    databaseSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'http')
+    databaseSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443), 'https')
     databaseSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(5432), 'postgres')
     databaseSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(27017), 'mongo')
 
-    const ec2Instance = new ec2.Instance(this, 'database-ec2', {
-      instanceName: 'database-ec2',
-      vpc,
-      vpcSubnets: {
-        subnetType: ec2.SubnetType.PUBLIC
-      },
-      securityGroup: databaseSecurityGroup,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T4G, ec2.InstanceSize.NANO),
-      machineImage: ec2.MachineImage.latestAmazonLinux({
-        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-        cpuType: ec2.AmazonLinuxCpuType.ARM_64
-      }),
-      keyName: 'cloud-key' // Key was manually created through console,
-    })
-
-    // Run an EC2 backup once a week and retain for a month
-    const plan = new backup.BackupPlan(this, 'database-backup-plan', {
-      backupPlanName: 'database-backup-plan',
-      backupPlanRules: [
-        new backup.BackupPlanRule({
-          ruleName: 'database-backup-plan-rule',
-          deleteAfter: cdk.Duration.days(35),
-          scheduleExpression: events.Schedule.cron({ weekDay: 'THU', hour: '5', minute: '0' })
-        })
-      ],
-      backupVault: new backup.BackupVault(this, 'database-backup-vault', {
+    // Manually set EC2 to use this backup plan as a safety insurance so that I don't have to go through setting up the server again
+    backup.BackupPlan.daily35DayRetention(
+      this,
+      'database-backup-plan',
+      new backup.BackupVault(this, 'database-backup-vault', {
         backupVaultName: 'database-backup-vault'
       })
-    })
-    plan.addSelection('database-plan-selection', {
-      backupSelectionName: 'database-plan-selection',
-      resources: [backup.BackupResource.fromEc2Instance(ec2Instance)]
-    })
+    )
 
-    new ec2.CfnEIP(this, 'database-elastic-ip', {
-      instanceId: ec2Instance.instanceId
-    })
+    // Manually associated this with EC2
+    const databaseEip = new ec2.CfnEIP(this, 'database-elastic-ip')
 
+    // More backup guarantees of individual databases
+    // On the EC2, there are daily cron scripts to backup database dumps to this S3 location
     new s3.Bucket(this, 'database-backup', {
       bucketName: 'maxrchung-database-backup', // Has to be globally unique
       lifecycleRules: [
@@ -82,20 +66,10 @@ export class MaxrchungCloudCdkStack extends cdk.Stack {
       zoneName: 'maxrchung.com'
     })
 
-    const balancer = new elbv2.ApplicationLoadBalancer(this, `cloud-application-load-balancer`, {
-      loadBalancerName: 'cloud-application-load-balancer',
-      vpc,
-      vpcSubnets: {
-        subnetType: ec2.SubnetType.PUBLIC
-      },
-      internetFacing: true,
-      idleTimeout: cdk.Duration.hours(1) // For longer running connections, e.g. THRUSTIN websockets
-    })
-
     new route53.ARecord(this, 'maxrchung-a-record', {
       recordName: 'maxrchung.com',
       zone: hostedZone,
-      target: route53.RecordTarget.fromAlias(new targets.LoadBalancerTarget(balancer))
+      target: route53.RecordTarget.fromIpAddresses(databaseEip.ref)
     })
 
     new route53.CnameRecord(this, 'maxrchung-cname-record', {
@@ -104,58 +78,16 @@ export class MaxrchungCloudCdkStack extends cdk.Stack {
       domainName: 'maxrchung.com'
     })
 
-    new route53.CnameRecord(this, 'maxrchung-cname-server-record', {
-      recordName: '*.server.maxrchung.com',
-      zone: hostedZone,
-      domainName: 'maxrchung.com'
-    })
-
-    new route53.CnameRecord(this, 'maxrchung-cname-api-record', {
-      recordName: '*.api.maxrchung.com',
-      zone: hostedZone,
-      domainName: 'maxrchung.com'
-    })
-
-    const certificate = new certificatemanager.Certificate(this, 'maxrchung-certificate', {
-      domainName: 'maxrchung.com',
-      validation: certificatemanager.CertificateValidation.fromDns(hostedZone),
-      subjectAlternativeNames: ['maxrchung.com', '*.maxrchung.com', '*.server.maxrchung.com', '*.api.maxrchung.com']
-    })
-
-    const listener = balancer.addListener('cloud-balancer-listener-https', {
-      open: true,
-      protocol: elbv2.ApplicationProtocol.HTTPS,
-      certificates: [certificate],
-      defaultAction: elbv2.ListenerAction.fixedResponse(404)
-    })
-
-    balancer.addListener('cloud-balancer-listener-http', {
-      open: true,
-      protocol: elbv2.ApplicationProtocol.HTTP,
-      defaultAction: elbv2.ListenerAction.redirect({
-        protocol: elbv2.ApplicationProtocol.HTTPS,
-        port: '443'
-      })
-    })
-
-    const cluster = new ecs.Cluster(this, 'cloud-cluster', {
-      clusterName: 'cloud-cluster',
+    const containersCluster = new ecs.Cluster(this, 'containers-cluster', {
+      clusterName: 'containers-cluster',
       vpc
     })
 
-    const taskDefinition = new ecs.FargateTaskDefinition(this, 'cloud-task-definition', {
-      family: 'cloud-task-family'
+    const containersTaskDefinition = new ecs.FargateTaskDefinition(this, 'containers-task-definition', {
+      family: 'containers-family'
     })
 
-    const fargate = new ecs.FargateService(this, 'cloud-fargate', {
-      serviceName: 'cloud-fargate',
-      cluster,
-      desiredCount: 1,
-      taskDefinition,
-      assignPublicIp: true
-    })
-
-    taskDefinition.addContainer('maxrchung-rails-container', {
+    containersTaskDefinition.addContainer('maxrchung-rails-container', {
       containerName: 'maxrchung-rails-container',
       image: ecs.ContainerImage.fromRegistry('maxrchung/maxrchung-rails'),
       environment: {
@@ -176,7 +108,7 @@ export class MaxrchungCloudCdkStack extends cdk.Stack {
       portMappings: [{ containerPort: 3000 }]
     })
 
-    taskDefinition.addContainer('thrustin-container', {
+    containersTaskDefinition.addContainer('thrustin-container', {
       containerName: 'thrustin-container',
       image: ecs.ContainerImage.fromRegistry('maxrchung/thrustin'),
       environment: {
@@ -192,7 +124,7 @@ export class MaxrchungCloudCdkStack extends cdk.Stack {
       portMappings: [{ containerPort: 3012 }]
     })
 
-    taskDefinition.addContainer('functional-vote-container', {
+    containersTaskDefinition.addContainer('functional-vote-container', {
       containerName: 'functional-vote-container',
       image: ecs.ContainerImage.fromRegistry('maxrchung/functional-vote'),
       environment: {
@@ -211,38 +143,51 @@ export class MaxrchungCloudCdkStack extends cdk.Stack {
       portMappings: [{ containerPort: 4000 }]
     })
 
-    fargate.registerLoadBalancerTargets(
-      {
-        containerName: 'maxrchung-rails-container',
-        newTargetGroupId: 'maxrchung-rails-target-group',
-        listener: ecs.ListenerConfig.applicationListener(listener, {
-          priority: 100,
-          conditions: [elbv2.ListenerCondition.hostHeaders(['maxrchung.com', 'www.maxrchung.com'])]
-        })
+    containersTaskDefinition.addContainer('retro-container', {
+      containerName: 'retro-container',
+      image: ecs.ContainerImage.fromRegistry('maxrchung/retro'),
+      environment: {
+        AWS_ACCESS_KEY_ID: ssm.StringParameter.valueForStringParameter(this, 'maxrchung-aws-access-key-id'),
+        AWS_SECRET_ACCESS_KEY: ssm.StringParameter.valueForStringParameter(this, 'maxrchung-aws-secret-access-key')
       },
-      {
-        containerName: 'thrustin-container',
-        newTargetGroupId: 'thrustin-target-group',
-        listener: ecs.ListenerConfig.applicationListener(listener, {
-          healthCheck: {
-            // There is no health check endpoint on the backend. When you try and hit the root,
-            // the backend is returning a 400 as it expects a websocket connection.
-            healthyHttpCodes: '200,400',
-            // Make logging less filled with web socket connection errors, max value is 300
-            interval: cdk.Duration.seconds(300)
-          },
-          priority: 200,
-          conditions: [elbv2.ListenerCondition.hostHeaders(['thrustin.server.maxrchung.com'])]
-        })
-      },
-      {
-        containerName: 'functional-vote-container',
-        newTargetGroupId: 'functional-vote-target-group',
-        listener: ecs.ListenerConfig.applicationListener(listener, {
-          priority: 300,
-          conditions: [elbv2.ListenerCondition.hostHeaders(['functionalvote.api.maxrchung.com'])]
-        })
+      logging: ecs.LogDriver.awsLogs({
+        logGroup: new logs.LogGroup(this, 'retro-log-group', {
+          logGroupName: 'retro-log-group',
+          retention: logs.RetentionDays.ONE_MONTH,
+          removalPolicy: cdk.RemovalPolicy.DESTROY
+        }),
+        streamPrefix: 'retro-log'
+      }),
+      portMappings: [{ containerPort: 5000 }],
+      healthCheck: {
+        // wget health check: https://stackoverflow.com/a/47722899
+        // Apollo Server health check endpoint: https://www.apollographql.com/docs/apollo-server/monitoring/health-checks/#http-level-health-checks
+        command: ['CMD-SHELL', 'wget --no-verbose --tries=1 --spider http://127.0.0.1:5000/.well-known/apollo/server-health || exit 1']
       }
-    )
+    })
+
+    const containersSecurityGroup = new ec2.SecurityGroup(this, 'containers-security-group', {
+      securityGroupName: 'containers-security-group',
+      vpc,
+      allowAllOutbound: true
+    })
+    containersSecurityGroup.addIngressRule(ec2.Peer.ipv4(databaseEip.ref + '/32'), ec2.Port.allTcp(), 'retro')
+
+    // Can't use EC2Service due to public access restrictions: https://stackoverflow.com/a/60885984
+    const containersService = new ecs.FargateService(this, 'containers-fargate', {
+      serviceName: 'containers-fargate',
+      cluster: containersCluster,
+      desiredCount: 1,
+      taskDefinition: containersTaskDefinition,
+      assignPublicIp: true,
+      cloudMapOptions: {
+        name: 'containers-cloud-options',
+        cloudMapNamespace: new servicediscovery.PrivateDnsNamespace(this, 'containers-namespace', {
+          name: 'containers.internal',
+          vpc
+        })
+      },
+      securityGroups: [containersSecurityGroup]
+    })
   }
 }
